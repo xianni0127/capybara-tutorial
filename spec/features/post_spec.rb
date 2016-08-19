e13257f84b4738963771e11274ae98cc2c38b68f
ni xian <nxian@CNnxian.local>
2016-08-19 16:41:13 +0800

    the second homework

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,21 +2,37 @@ require 'rails_helper'
 
 RSpec.feature 'post feature', type: :feature do
 
-  scenario 'create new post' do
+  scenario 'create/edit/display/destroy new post' do
     visit '/posts'
-    sleep(5)
     on_page_with :post_list do |page|
-        expect(page.posts_in_list.length).to eq(0)
-        sleep(1)
-        page.new_post_link.click
-        sleep(1)
-        page.perform :fill_post, 'my first post', 'I wanna be the full-stack dev'
-        sleep(1)
-        page.create_post_button.click
-        sleep(1)
-        expect(page.notice_message).to eq('Post was successfully created.')
-        page.back_link.click
-        expect(page.posts_in_list.length).to eq(1)
-    end
+    expect(page.posts_in_list.length).to eq(0)
+    page.new_post_link.click
+    page.perform :fill_post, 'my first post', 'I wanna be the full-stack dev'
+    page.create_post_button.click
+    expect(page.notice_message).to eq('Post was successfully created.')
+    page.back_link.click
+    expect(page.posts_in_list.length).to eq(1)
+    page.perform :verify_post_data, {:line=>1, :title=>'my first post', :content=>'I wanna be the full-stack dev'}
+
+    page.perform :edit_link_click, {:line=>1}
+    page.perform :fill_post, 'my second post', 'I wanna be the front edage dev'
+    page.update_post_button.click
+    page.back_link.click
+    expect(page.posts_in_list.length).to eq(1)
+    page.perform :verify_post_data, {:line=>1, :title=>'my second post', :content=>'I wanna be the front edage dev'}
+
+    page.perform :show_link_click, {:line=>1}
+    page.perform :verify_show_data, {:title=>'my second post', :content=>'I wanna be the front edage dev'}
+    page.back_link.click
+    expect(page.posts_in_list.length).to eq(1)
+
+    page.perform :destroy_link_click, {:line=>1}
+    page.cancel_destroy
+    expect(page.posts_in_list.length).to eq(1)
+    page.perform :destroy_link_click, {:line=>1}
+    page.accept_destroy
+    expect(page.posts_in_list.length).to eq(0)
   end
+  end
+
 end
--- spec/pages/page_with_post_list.rb
+++ spec/pages/page_with_post_list.rb
@@ -5,7 +5,7 @@ module PageWithPostList
 		has_css?('.parent .left')
 	end
 
-	def posts_in_list
+  def posts_in_list
 		find('.parent .left table').find('tbody').all('tr')
 	end
 
@@ -17,6 +17,10 @@ module PageWithPostList
 		find('.actions input[value="Create Post"]')
 	end
 
+	def update_post_button
+		find('.actions input[value="Update Post"]')
+	end
+
 	def notice_message
 		find('#notice').text
 	end
@@ -25,8 +29,50 @@ module PageWithPostList
 		find('a[href="/posts"]')
 	end
 
+  define_action :edit_link_click do |index|
+		find_all('tr')[index[:line]].all('td')[4].click
+	end
+
+	define_action :show_link_click do |index|
+		find_all('tr')[index[:line]].all('td')[3].click
+	end
+
+	define_action :destroy_link_click do |index|
+		find_all('tr')[index[:line]].all('td')[5].click
+	end
+
 	define_action :fill_post do |title, content|
 		fill_in('post_title', with: title)
 		fill_in('post_content', with: content)
 	end
+
+	define_action :verify_post_title do |index, title|
+		expect(find_all('tr')[index[:line]].all('td')[0].text).to eq(title)
+	end
+
+	define_action :verify_post_content do |index, content|
+		expect(find_all('tr')[index[:line]].all('td')[1].text).to eq(content)
+
+	end
+
+	define_action :verify_post_data do |index|
+		expect(find_all('tr')[index[:line]].all('td')[0].text).to eq(index[:title])
+		expect(find_all('tr')[index[:line]].all('td')[1].text).to eq(index[:content])
+	end
+
+	define_action :verify_show_data do |args|
+		expect(has_text?(args[:content])).to eq(true)
+		expect(has_text?(args[:title])).to eq(true)
+
+	end
+
+	 def accept_destroy
+        page.driver.browser.switch_to.alert.accept
+    end
+
+    def cancel_destroy
+        page.driver.browser.switch_to.alert.dismiss
+    end
+
+
 end
